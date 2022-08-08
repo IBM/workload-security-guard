ccd0380218690d0761c8cdfa4508f4f63e354b40
David Hadas <david.hadas@gmail.com>
2022-08-08 18:39:55 +0300

    preperations for security-guard

--- scripts/bx/login.sh
+++
@@ -1,6 +0,0 @@
-#bx login --sso
-bx login -c 8cb197d2d1424b5195f365ec6ca188cc -r us-south --sso
-bx cr login
-bx cs cluster config --cluster KN1
-bx target -g Default
-kubectl config use-context guestbook-context
